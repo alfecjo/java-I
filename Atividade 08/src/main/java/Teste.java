8eed028b0a88fe811da501071b3bb89b3eb4da90
Antonio Almeida <alfecjo@hotmail.com>
2023-06-26 17:56:44 -0300

    As entregas corespondem a um projeto inicial e sua evolução até o final do módulo

---
+++ Atividade 08/src/main/java/Teste.java
@@ -0,0 +1,18 @@
+
+import javax.swing.JTextField;
+
+public class Teste extends javax.swing.JFrame {
+
+    public static void main(String[] args) {
+        com.netbeans.project.exerc09.JFMenuTelaPrincipal jFMenuTelaPrincipal = com.netbeans.project.exerc09.JFMenuTelaPrincipal.criaJFMenuTelaPrincipal();
+//        com.netbeans.project.exerc09.JFMenuVeicPasseio jFMunuVeicPasseio = com.netbeans.project.exerc09.JFMenuVeicPasseio.criaJFMenuVeicPasseio();
+//        com.netbeans.project.exerc09.JFMenuVeicCarga jFMunuVeicCarga = com.netbeans.project.exerc09.JFMenuVeicCarga.criaJFMenuVeicCarga();
+//        com.netbeans.project.exerc09.JFCadastroPasseio jFCadastroPasseio = com.netbeans.project.exerc09.JFCadastroPasseio.criaJFCadastroPasseio();
+//        com.netbeans.project.exerc09.JFCadastroCarga jFCadastro = com.netbeans.project.exerc09.JFCadastroCarga.criaJFCadastroCarga();
+//        com.netbeans.project.exerc09.JFConsultarExcluirPlacaPasseio jFConsultarExcluirPlacaPasseio = com.netbeans.project.exerc09.JFConsultarExcluirPlacaPasseio.criaJFConsultarExcluirPlacaPasseio();
+//        com.netbeans.project.exerc09.JFConsultarExcluirPlacaCarga jFConsultarExcluirPlacaCarga = com.netbeans.project.exerc09.JFConsultarExcluirPlacaCarga.criaJFConsultarExcluirPlacaCarga();
+//        com.netbeans.project.exerc09.JFImprimirExcluirTodosPasseio JFImprimirExcluirTodosPasseio = com.netbeans.project.exerc09.JFImprimirExcluirTodosPasseio.criaJFImprimirExcluirTodosPasseio();
+//        com.netbeans.project.exerc09.JFImprimirExcluirTodosCarga jFImprimirExcluirTodosCarga = com.netbeans.project.exerc09.JFImprimirExcluirTodosCarga.criaJFImprimirExcluirTodosCarga();
+
+    }
+}
