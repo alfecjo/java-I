d740b0371942fa06d8f3637326aa50897b4e9963
alfecjo <alfecjo@hotmail.com>
2024-01-11 09:04:15 -0300

    Atividades executadas no VSCode Ok!

--- Atividade 07/src/com/ex06/Calcular.java
+++
@@ -1,8 +0,0 @@
-//pacote ex_06 - todas as classes do projeto estao contidas no mesmo pacote
-package com.ex06;
-
-//interface em conformidade com as especificacoes do sistema
-public interface Calcular {
-    int calcular();
-}
-//Fim da interface
